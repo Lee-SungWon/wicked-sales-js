3b72668371219cad2d9bb5712d9c09a0d2e729e5
Chris Sung Won Lee <sungwlee8@gmail.com>
2020-10-18 00:16:38 +0000

    changed name

--- client/components/Header.jsx
+++ client/components/Header.jsx
@@ -16,8 +16,8 @@ class Header extends React.Component {
       <header>
         <div className="d-flex justify-content-between">
           <h3 className="pointer ml-5" onClick={() => this.props.setView('catalog', {})}>
-            <i className="mr-2 fas fa-dollar-sign"></i>
-            Wicked Sales
+            <i className="mr-2 fas fa-baby-carriage"></i>
+            Babies N Us
           </h3>
           <div className="mr-5 header-cart pointer" onClick={this.handleCartClick}>
             <span>{this.props.cartItemCount} Items</span>
